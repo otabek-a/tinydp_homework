57e4170b21af27c7c6786b1423fb1ced3943120f
otabek-a <otabekabdurasulov245@gmail.com>
2025-01-25 20:28:00 +0500

    dacumented 20 random people

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from tinydb import TinyDB
-import tinydb
-db=TinyDB('table.json')
+import requests
+doc=TinyDB('db.json')
+db=TinyDB('tabel.json')
 user={
-    'first name':'otabek',
-    'last name':'Abdurasulov',
+    'name':'Otabek',
     'age':18,
-    'job':'student',
+    'surname':'otabek',
+    'job':'student'
 }
 user2={
-    'first name':'sardor',
-    'last name':'muminjonov',
+    'name':'sardor',
     'age':18,
-    'job':'student',
+    'surname':'muminjonov',
+    'job':'student'
 }
 db.insert(user)
 db.insert(user2)
+url = 'https://randommer.io/api/Name?nameType=fullname&quantity=20'
+headers={
+    'X-Api-Key':'41c1434e36c349c3972448fca049b6e3'
+}
+response = requests.get(url,headers=headers)
+r = response.json()
+for i in r:
+    doc.insert({'name':f'{i}'})
+
