978c2fb2ac201a95bc283978838d05db13de11fa
otabek-a <otabekabdurasulov245@gmail.com>
2025-01-26 11:10:00 +0500

    items of shop

---
+++ shopbase.py
@@ -0,0 +1,20 @@
+shopbase = {
+    'laptops': [
+        {'name': 'Macbook Pro 16 (2023)', 'model': 'M2Pro', 'memory': '512 GB SSD', 'cost': '2.49$'},
+        {'name': 'DELL XPS 15', 'model': '16GB RAM', 'memory': '16GB RAM', 'cost': '1.899$'},
+        {'name': 'HP Spectre 360', 'model': '360', 'memory': '512GB SSD 16GB RAM', 'cost': '1.299$'},
+        {'name': 'Lenovo IdeaPad', 'model': 'IdeaPad 5', 'memory': '256GB SSD 8GB RAM', 'cost': '649$'}
+    ],
+    'phones': [
+        {'name': 'iPhone', 'model': '14 Pro Max', 'memory': '256 GB', 'cost': '1.199$'},
+        {'name': 'Samsung', 'model': 'Galaxy S23 Ultra', 'memory': '512 GB', 'cost': '1.399$'},
+        {'name': 'Xiaomi', 'model': 'Redmi Note 13 Pro', 'memory': '256 GB', 'cost': '399$'},
+        {'name': 'Google', 'model': 'Pixel 8 Pro', 'memory': '128 GB', 'cost': '999$'}
+    ],
+    'tables': [
+        {'name': 'iPad', 'model': 'Pro 12.9 (2023)', 'memory': '256 GB', 'cost': '1.099$'},
+        {'name': 'Samsung', 'model': 'Galaxy Tab S9 Ultra', 'memory': '512 GB', 'cost': '1.999$'},
+        {'name': 'Xiaomi', 'model': 'Pad 6 Pro', 'memory': '256 GB', 'cost': '499$'},
+        {'name': 'Lenovo', 'model': 'Tab P12 Pro', 'memory': '128 GB', 'cost': '699$'}
+    ]
+}
